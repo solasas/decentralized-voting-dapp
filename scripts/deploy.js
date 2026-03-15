f2a9528c90feba0a27283071bf20af396cfa73f8
sashank solasa <sashanksolasa@gmail.com>
2026-03-15 21:41:35 +0530

    after deploying the samrt contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,14 @@
-const hre = require("hardhat");
+import hre from "hardhat";
 
 async function main() {
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(Date.now() + 60 * 60 * 24 * 1000, { value: hre.ethers.parseEther("1") });
-  await lock.waitForDeployment();
-  console.log(`Lock deployed to: ${await lock.getAddress()}`);
+
+  const Voting = await hre.ethers.getContractFactory("Create");
+
+  const voting = await Voting.deploy();
+
+  await voting.waitForDeployment();
+
+  console.log(`Voting contract deployed to: ${await voting.getAddress()}`);
 }
 
 main().catch((error) => {
